383086e742c18ea44b91192d7514cb7362185d8c
dhyan-aranha <51273141+dhyan-aranha@users.noreply.github.com>
2025-04-17 12:09:45 +0200

    Update Monsky.lean

--- Monsky.lean
+++ Monsky.lean
@@ -1,3 +1,13 @@
 -- This module serves as the root of the `Monsky` library.
 -- Import modules here that should be built as part of the library.
-import Monsky.Basic
+import Monsky.Appendix
+import Monsky.Rainbow_triangles
+import Monsky.Triangle_corollary
+import Monsky.basic_definitions
+import Monsky.main_statment
+import Monsky.miscellaneous
+import Monsky.monsky_even
+import Monsky.segment_counting
+import Monsky.segment_triangle
+import Monsky.simplex_basic
+import Monsky.square
